072733a789ee2e07a97ec438751502dabf875347
R V <r.valler@yahoo.co.uk>
2023-07-08 11:43:29 +0200

    updated copyright date

--- src/neuron.h
+++ src/neuron.h
@@ -1,5 +1,5 @@
 /*****************************************************************
- * Copyright (C) 2017-2022 Robert Valler - All rights reserved.
+ * Copyright (C) 2017-2023 Robert Valler - All rights reserved.
  *
  * This file is part of the project: Learning
  *
